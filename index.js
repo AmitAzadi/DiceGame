6897e176bf11d6ab0027dca3a8c74f4b6e0add5f
Amit Kumar <56012894+AmitAzadi@users.noreply.github.com>
2021-03-07 12:23:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomnumber1=Math.floor(Math.random()*6)+1;
-var imagesource1="images/dice"+randomnumber1+".png";
+var imagesource1="dice"+randomnumber1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",imagesource1);
 
 var randomnumber2=Math.floor(Math.random()*6)+1;
-var imagesource2="images/dice"+randomnumber2+".png";
+var imagesource2="dice"+randomnumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",imagesource2);
 
 if(randomnumber1>randomnumber2)
